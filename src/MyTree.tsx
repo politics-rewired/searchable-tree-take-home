8b370165f31089f45196adbcaf437779f1238b15
Tom Brooks <d3vkit@gmail.com>
2021-12-05 17:04:56 -0800

    Change sample data to props data

--- src/MyTree.tsx
+++ src/MyTree.tsx
@@ -2,8 +2,8 @@ import { Tree } from "antd";
 import { Schema } from "./data";
 import {
   FormOutlined,
-  TableOutlined,
-  FilterOutlined,
+  // TableOutlined,
+  // FilterOutlined,
   NumberOutlined,
 } from "@ant-design/icons";
 import React from "react";
@@ -13,30 +13,59 @@ type MyTreeProps = {
 };
 
 const FormIcon = FormOutlined;
-const TableIcon = TableOutlined;
-const ViewIcon = FilterOutlined;
+// const TableIcon = TableOutlined;
+// const ViewIcon = FilterOutlined;
 const ColumnIcon = NumberOutlined;
 
-// If you don't want to use typescript, use line 10 instead of 11 and comment out the type `MyTreeProps` above
-// export const MyTree = (props) => {
 export const MyTree = (props: MyTreeProps) => {
-  const sampleData = [
-    {
-      title: "Sample Data",
-      key: "sample",
+  const data = props.data.map((schema, schemaIndex) => {
+    return {
+      title: schema.displayName,
+      key: `${schema.systemName}-${schemaIndex}-node`,
       icon: <FormIcon />,
-      children: [
-        { title: "Sample Child 1", key: "sample child 1", icon: <ViewIcon /> },
-        {
-          title: "Sample Child 2",
-          key: "sample child 2",
-        },
-      ],
-    },
-  ];
+      children: schema.tables.map((table) => {
+        return {
+          title: table.displayName,
+          key: `${schema.systemName}-${table.systemName}-table`,
+          children: [
+            {
+              title: "Forms",
+              key: `${schema.systemName}-${table.systemName}-forms`,
+              children: table.forms.map((form) => {
+                return {
+                  title: form.displayName,
+                  key: `${schema.systemName}-${table.systemName}-${form.systemName}`
+                }
+              }),
+            },
+            {
+              title: "Views",
+              key: `${schema.systemName}-${table.systemName}-views`,
+              children: table.views.map((view) => {
+                return {
+                  title: view.displayName,
+                  key: `${schema.systemName}-${table.systemName}-${view.systemName}`
+                }
+              }),
+            },
+            {
+              title: "Columns",
+              key: `${schema.systemName}-${table.systemName}-cols`,
+              children: table.columns.map((column, colIndex) => {
+                return {
+                  title: column.displayName,
+                  key: `col-${column.systemName}-${colIndex}`,
+                  icon: <ColumnIcon />
+                }
+              })
+            }
+          ]
+        }
+      })
+    };
+  });
 
-  // TODO - replace sampleData with testData transformed from props
-  const treeData = sampleData;
+  const treeData = data;
 
   return <Tree showIcon treeData={treeData} autoExpandParent={true} />;
 };
